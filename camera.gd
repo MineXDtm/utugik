d262bcc05703b37abe7e4bb478cdf7150e4abdbe
lodo4nik <rompetaalex@gmail.com>
2022-01-01 18:10:00 +0300

    my3Dpart

---
+++ camera.gd
@@ -0,0 +1,11 @@
+extends Camera
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if Input.is_action_pressed(cam_right):
