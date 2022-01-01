782104334377c07563a1b87d02546e48a4498fd9
lodo4nik <rompetaalex@gmail.com>
2022-01-01 18:58:04 +0300

    cameracontrolwithbuttons

--- camera.gd
+++ camera.gd
@@ -1,5 +1,6 @@
 extends Camera
 
+<<<<<<< Updated upstream
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -8,3 +9,18 @@ extends Camera
 
 if Input.is_action_pressed(cam_right):
 	velocity += -transform.basis.x * speed
+=======
+func _process(delta):
+	if Input.is_action_pressed("ui_left"):
+		rotation.y += 0.01
+	if Input.is_action_pressed("ui_right"):
+		rotation.y -= 0.01
+		
+	if rotation.y < -2.9:
+		rotation.y = -2.9
+	if rotation.y > -0.15:
+		rotation.y = -0.15
+
+func _on_right_button_down():
+	rotation.y -= 0.01
+>>>>>>> Stashed changes
