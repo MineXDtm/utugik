fc37b2e27422f20db2c0fe6d783f0da6d17be292
andreu323 <46608839+andreu323@users.noreply.github.com>
2022-01-01 19:21:14 +0200

    change

--- camera.gd
+++ camera.gd
@@ -1,15 +1,11 @@
 extends Camera
 
-<<<<<<< Updated upstream
+
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-
-if Input.is_action_pressed(cam_right):
-	velocity += -transform.basis.x * speed
-=======
 func _process(delta):
 	if Input.is_action_pressed("ui_left"):
 		rotation.y += 0.01
@@ -21,6 +17,3 @@ func _process(delta):
 	if rotation.y > -0.15:
 		rotation.y = -0.15
 
-func _on_right_button_down():
-	rotation.y -= 0.01
->>>>>>> Stashed changes
