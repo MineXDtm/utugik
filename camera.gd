5479a01601abe9c8c73eff19cbbf27ef98ebf428
lodo4nik <rompetaalex@gmail.com>
2022-01-01 20:53:35 +0300

    camerasmenu

--- camera.gd
+++ camera.gd
@@ -17,3 +17,13 @@ func _process(delta):
 	if rotation.y > -0.15:
 		rotation.y = -0.15
 
+	if rotation.y < -2.4:
+		get_parent().get_node("Control/cameras").visible=false
+	elif rotation.y > -0.7:
+		get_parent().get_node("Control/cameras").visible=false
+	else:
+		get_parent().get_node("Control/cameras").visible=true
+
+
+func _on_cameras_pressed():
+	pass # Replace with function body.
