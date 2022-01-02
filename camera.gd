aabede4b67cde87fd87d14312efdfa61db2bf480
lodo4nik <rompetaalex@gmail.com>
2022-01-02 11:55:43 +0300

    camerapopup
    camerapopup
    new camera icon
    loaded map

--- camera.gd
+++ camera.gd
@@ -12,8 +12,10 @@ func _process(delta):
 		rotation.y = -0.15
 	if rotation.y < -2.3:
 		get_parent().get_node("MeshInstance").visible=false
+		get_parent().get_node("MeshInstance/cameraswindow").visible=false
 	elif rotation.y > -2:
 		get_parent().get_node("MeshInstance").visible=false
+		get_parent().get_node("MeshInstance/cameraswindow").visible=false
 	else:
 		get_parent().get_node("MeshInstance").visible=true
 
@@ -22,3 +24,4 @@ func _on_Area_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed == true:
 			print("work")
+			get_parent().get_node("MeshInstance/cameraswindow").popup()
